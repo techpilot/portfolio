d6d265911912bf8be81c23f6bf272b6568c1ede3
Stephen Ngwu <ngwustephen99@gmail.com>
2022-11-03 07:26:10 +0100

    portfolio updates

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,7 +3,7 @@ import React from "react";
 const About = ({ data }) => {
   if (data) {
     var name = data.name;
-    var profilepic = "images/" + data.image;
+    var pic = "images/" + data.image;
     var bio = data.bio;
     var street = data.address.street;
     var city = data.address.city;
@@ -18,11 +18,7 @@ const About = ({ data }) => {
     <section id="about">
       <div className="row">
         <div className="three columns">
-          <img
-            className="profile-pic"
-            src={profilepic}
-            alt="Sonny's Profile Pic"
-          />
+          <img className="profile-pic" src={pic} alt="Stephen's Profile Pic" />
         </div>
         <div className="nine columns main-col">
           <h2>About Me</h2>
